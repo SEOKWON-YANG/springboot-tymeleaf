53fbc2ae013263dc90e4f9e13e3e0c6e80f28734
SEOKWON-YANG <namugocps@gmail.com>
2021-12-28 23:09:11 +0900

    Initial commit

---
+++ src/main/java/com/example/springboottymeleaf/SpringbootTymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottymeleaf/SpringbootTymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

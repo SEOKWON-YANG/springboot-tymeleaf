282ba071bd0c2f04e6f88672f6aee1673b9fa8f2
SEOKWON-YANG <namugocps@gmail.com>
2021-12-30 23:53:13 +0900

    스프링 Thymeleaf
    스프링 Thymeleaf
    
    - Thymeleaf 기본 사용법

---
+++ src/main/java/com/example/springboottymeleaf/dto/SampleDTO.java
@@ -0,0 +1,19 @@
+package com.example.springboottymeleaf.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+
+    private String first;
+
+    private String las;
+
+    private LocalDateTime regTime;
+}
